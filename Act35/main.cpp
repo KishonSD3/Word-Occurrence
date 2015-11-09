c8ce62909e9e7e46e20c50b1a2e5d66ffbbdbd6b
Kishon Daniels <kishondaniels@gmail.com>
2015-11-08 18:55:46 -0600

    Word Occurrence
    A program which reads this string and find each occurrence of the word
    “love”, and then changes the word “love” to “hate”. Displays new
    paragraph on the screen.

--- Act35/main.cpp
+++ Act35/main.cpp
@@ -4,12 +4,43 @@
 //
 //  Created by Kishon Daniels on 11/3/15.
 //  Copyright © 2015 HackStack. All rights reserved.
-//
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+
+{
+    
+string paragraph = "I love your dark eyes,\n"
+                   "And your curly hair, \n"
+                    "I love your smile \n"
+                   "And the way you care,\n"
+                   "I love your deep kisses, \n"
+                   "I love your soft touch, \n"
+                   "I love you, I love you, \n"
+                   "I love you so much \n";
+
+    cout << paragraph << endl;
+
+    int position = paragraph.size();
+    
+    while (1)
+        
+    {
+        position = paragraph.rfind("love", position -1);
+        
+        if (position==string::npos)
+            
+            break;
+        
+        cout << position << endl;
+        
+        paragraph.replace(position,4, "hate");
+        
+        cout << paragraph << endl;
+    }
+    
     return 0;
 }
